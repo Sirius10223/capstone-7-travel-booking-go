438b1020d7602994afdee8161ce1a10fd6513d67
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2023-04-14 23:25:13 +0700

    Create order_controller.go

---
+++ controllers/order_controller.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"github.com/labstack/echo/v4"
+	""
+)
+
+type OrderTrainsController struct {
+}
+
+type OrderHotelsController struct {
+}
+
+func NewOrderTrainsController() OrderTrainsController {
+
+}
+
+func NewOrderHotelsController() OrderHotelsController {
+
+}
