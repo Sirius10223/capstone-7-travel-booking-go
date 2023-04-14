9d856fdac3460e09f4c906507763cf60a0ab60e5
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2023-04-14 23:20:09 +0700

    Create login_controller.go

---
+++ controllers/login_controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type LoginController struct {
+}
+
+type UserController struct {
+}
+
+func NewUserController() UserController {
+
+}
+
+func (us UserController) Login(c echo.Context) error {
+
+}
